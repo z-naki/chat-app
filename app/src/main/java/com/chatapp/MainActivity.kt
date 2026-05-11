7cca171ac7537be68bcde3a580ac9b08a5a986e1
Developer <developer@chatapp.dev>
2026-05-11 15:22:25 +0800

    feat: add Application and MainActivity entry points

---
+++ app/src/main/java/com/chatapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.chatapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.chatapp.ui.navigation.NavGraph
+import com.chatapp.ui.theme.ChatAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatAppTheme {
+                NavGraph()
+            }
+        }
+    }
+}
