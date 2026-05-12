ad88c88e215a3a3cf99dbeae81ba663aba9c5e75
Developer <developer@chatapp.dev>
2026-05-12 11:54:49 +0800

    v0.0.1-alpha: fix LocalFocusManager import

--- app/src/main/java/com/chatapp/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/chatapp/ui/chat/ChatScreen.kt
@@ -44,6 +44,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
